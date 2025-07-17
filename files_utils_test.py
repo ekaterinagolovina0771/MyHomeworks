5fe9ec4b383009d3a89061e8d58a4c815543adf1
kategolovina84 <kategolovina84@yandex.ru>
2025-07-17 18:39:26 +0500

    hw_22: Тестирование функций

---
+++ files_utils_test.py
@@ -0,0 +1,213 @@
+"""
+HOME WORK 22
+Функции для работы с файлами различных форматов: JSON, CSV, TXT и YAML
+"""
+
+import json
+import csv
+import yaml  # pip install pyyaml
+
+# Функции для работы с JSON:
+
+
+def read_json(file_path: str, encoding: str = "utf-8"):
+    """
+    Функция для чтения данных из JSON-файла.
+    :param file_path: путь к файлу
+    :param encoding: кодировка файла
+    # :return: список словарей
+    """
+    with open(file_path, "r", encoding=encoding) as file:
+        data = json.load(file)
+        return data
+
+
+def write_json(file_path: str, *data: dict, encoding: str = "utf-8") -> None:
+    """
+    Функция для записи данных в JSON-файл.
+    :param file_path: путь к файлу
+    :param data: данные для записи
+    :param encoding: кодировка файла
+    """
+    with open(file_path, "w", encoding=encoding) as file:
+        json.dump(data, file, indent=4, ensure_ascii=False)
+
+
+def append_json(file_path: str, *new_data: dict, encoding: str = "utf-8") -> None:
+    """
+    Функция для добавления данных в JSON-файл.
+    :param file_path: путь к файлу
+    :param data: данные для записи
+    :param encoding: кодировка файла
+    """
+    data_json = read_json(file_path)
+    data_json.extend(new_data)
+    write_json(file_path, *data_json)
+
+
+# Функции для работы с CSV:
+
+
+def read_csv(
+    file_path: str, delimiter: str = ";", encoding: str = "utf-8-sig"
+) -> list[dict]:
+    """
+    Функция для чтения данных из CSV-файла.
+    :param file_path: путь к файлу
+    :param delimiter : разделитель
+    :param encoding: кодировка файла
+    :return: список словарей
+
+    """
+    with open(file_path, "r", encoding=encoding) as file:
+        data = csv.DictReader(file, delimiter=delimiter)
+        return list(data)
+
+
+def write_csv(
+    file_path: str, *data: dict, delimiter: str = ";", encoding: str = "utf-8-sig"
+) -> None:
+    """
+    Функция для записи данных в CSV-файл.
+    :param data: данные для записи
+    :param file_path: путь к файлу
+    :param delimiter : разделитель
+    :param encoding: кодировка файла
+    """
+    with open(file_path, "w", encoding=encoding, newline="") as file:
+        writer = csv.DictWriter(file, fieldnames=data[0].keys(), delimiter=";")
+        writer.writeheader()
+        writer.writerows(data)
+
+
+def append_csv(*new_data: dict, file_path: str, encoding: str = "utf-8-sig") -> None:
+    """
+    Функция для добавления данных в конец CSV-файла.
+    :param data: данные для записи
+    :param file_path: путь к файлу
+    :param encoding: кодировка файла
+    """
+    with open(file_path, "a", encoding=encoding, newline="") as file:
+        writer = csv.DictWriter(file, fieldnames=data[0].keys(), delimiter=";")
+        writer.writerows(new_data)
+
+
+# Функции для работы с TXT:
+
+
+def read_txt(file_path: str, encoding: str = "utf-8") -> list[str]:
+    """
+    Функция для чтения текстового документа.
+    :param file_path: путь к файлу
+    :param encoding: кодировка файла
+    :return: список строк
+    """
+    with open(file_path, "r", encoding=encoding) as file:
+        data = [line.rstrip() + "\n" for line in file.readlines()]
+        return data
+
+
+def write_txt(file_path: str, *data: str, encoding: str = "utf-8") -> None:
+    """
+    Функция для записи текстового документа.
+    :param data: данные для записи
+    :param file_path: путь к файлу
+    :param encoding: кодировка файла
+    """
+    with open(file_path, "w", encoding=encoding) as file:
+        ready_data = [line.rstrip() + "\n" for line in data]
+        file.writelines(ready_data)
+
+
+def append_txt(file_path: str, *new_data: str, encoding: str = "utf-8") -> None:
+    """
+    Функция для добавления данных в конец текстового документа.
+    :param data: данные для записи
+    :param file_path: путь к файлу
+    :param encoding: кодировка файла
+    """
+    with open(file_path, "a", encoding=encoding) as file:
+        ready_new_data = [line.rstrip() + "\n" for line in new_data]
+        file.writelines(ready_new_data)
+
+
+# Функции для работы с YAML:
+# Чтение YAML файла
+
+
+def read_yaml(file_path: str, encoding: str = "utf-8"):
+    """
+    Функция для чтения YAML файла:
+    :param file_path: путь к файлу
+    :param encoding: кодировка файла
+    """
+    with open(file_path, "r", encoding=encoding) as file:
+        data = yaml.safe_load(file)
+        return data
+    
+# Данные для тестирования работы с JSON, CSV
+data = [
+    {
+        "name": "Nothing Else Matters",
+        "author": "James Hetfield, Lars Ulrich",
+        "year": 1991,
+        "duration": 390,  # 6 минут 30 секунд
+        "description": "Одна из самых известных баллад Metallica, написанная Джеймсом Хэтфилдом. Песня о доверии и преданности.",
+    },
+    {
+        "name": "Enter Sandman",
+        "author": "James Hetfield, Lars Ulrich, Kirk Hammett",
+        "year": 1991,
+        "duration": 332,  # 5 минут 32 секунды
+        "description": "Хит из альбома 'Metallica', ставший визитной карточкой группы. Песня о кошмарах и страхах.",
+    },
+    {
+        "name": "Master of Puppets",
+        "author": "James Hetfield, Lars Ulrich, Kirk Hammett, Cliff Burton",
+        "year": 1986,
+        "duration": 515,  # 8 минут 35 секунд
+        "description": "Эпическая композиция из одноимённого альбома, классика трэш-метала. Песня о зависимости и контроле.",
+    },
+]
+
+new_data = [
+    {
+        "name": "One",
+        "author": "James Hetfield, Lars Ulrich",
+        "year": 1988,
+        "duration": 446,  # 7 минут 26 секунд
+        "description": "Мощная песня из альбома '...And Justice for All', известная своим социальным посылом. Песня о войне и её последствиях.",
+    },
+    {
+        "name": "Fade to Black",
+        "author": "James Hetfield, Lars Ulrich, Kirk Hammett, Cliff Burton",
+        "year": 1984,
+        "duration": 420,  # 7 минут 0 секунд
+        "description": "Лирическая композиция из альбома 'Ride the Lightning', одна из первых баллад группы. Песня о депрессии и потере смысла жизни.",
+    },
+]
+# Запись данных в JSON файл
+file_path = 'test.json'
+write_json(file_path, *data)
+append_json(file_path, *new_data)
+print(read_json(file_path))
+
+# Запись данных в CSVфайл
+file_path = 'test.csv'
+write_csv(file_path, *data)
+append_csv(*new_data, file_path=file_path)
+print(read_csv(file_path))
+
+# Данные для тестирования работы с TXT
+file_path = "test.txt"
+data = 'Привет, мир!'
+new_data = 'Hi, word!'
+
+write_txt(file_path, data)
+append_txt(file_path, new_data)
+print(read_txt(file_path))
+
+# Тестирование работычтения файла формата YAML
+file_path = 'test.yaml'
+data = read_yaml(file_path)
+print(data)
